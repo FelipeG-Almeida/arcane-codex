3b82b7474be7590810fb371ccd6224f74df6ca68
Felipe Almeida <felipegustavotheboss@gmail.com>
2023-06-01 17:51:13 -0300

    Funções criptografar e descriptografar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const data = document.getElementsByClassName('data')[0];
 const empty = document.getElementsByClassName('empty')[0];
-const output = document.getElementById('output')
+const output = document.getElementById('output');
 const password = {
 	a: 'ai',
 	e: 'enter',
@@ -9,18 +9,49 @@ const password = {
 	u: 'ufat',
 };
 
-window.onload = () => {
-	data.style.display = 'none';
-};
+function showResult(text) {
+	if (text !== '') {
+		empty.classList.remove('fade-in');
+		empty.classList.add('fade-out', 'animated');
+		data.classList.remove('fade-out');
+		data.classList.add('fade-in', 'animated');
+		data.classList.remove('hide');
+		empty.classList.add('hide');
+	} else {
+		data.classList.remove('fade-in');
+		data.classList.add('fade-out', 'animated');
+		empty.classList.remove('fade-out');
+		empty.classList.add('fade-in', 'animated');
+		empty.classList.remove('hide');
+		data.classList.add('hide');
+	}
+}
 
 function encrypt() {
 	let plaintext = document.getElementsByTagName('textarea')[0].value;
+
+	showResult(plaintext)
 
 	plaintext = plaintext.replace(/a|e|i|o|u/gi, function (match) {
 		return password[match];
 	});
 
-	empty.style.display = 'none';
-    data.style.display = 'block';
-    output.innerHTML = plaintext
+	output.innerHTML = plaintext;
+}
+
+function decrypt() {
+	let plaintext = document.getElementsByTagName('textarea')[0].value;
+
+	showResult(plaintext)
+
+	const reversedPassword = {};
+	Object.entries(password).forEach(([key, value]) => {
+		reversedPassword[value] = key;
+	});
+
+	plaintext = plaintext.replace(/ai|enter|imes|ober|ufat/gi, function (match) {
+		return reversedPassword[match];
+	});
+
+	output.innerHTML = plaintext;
 }
