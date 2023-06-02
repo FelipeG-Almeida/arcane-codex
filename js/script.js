b5b97f23e38bbf15e6d3284d20546784d01f5b8d
Felipe Almeida <felipegustavotheboss@gmail.com>
2023-06-02 17:55:35 -0300

    Função Copiar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const data = document.getElementsByClassName('data')[0];
 const empty = document.getElementsByClassName('empty')[0];
+const input = document.getElementsByTagName('textarea')[0];
 const output = document.getElementById('output');
+const copyButon = document.getElementsByClassName('button__3')[0];
 const password = {
 	a: 'ai',
 	e: 'enter',
@@ -9,28 +11,29 @@ const password = {
 	u: 'ufat',
 };
 
+input.addEventListener('keypress', function (event) {
+	let key = event.key;
+	let regex = new RegExp('^[a-zA-Z0-9s ]+$');
+
+	if (!regex.test(key)) {
+		event.preventDefault();
+		return false;
+	}
+});
+
 function showResult(text) {
-	if (text !== '') {
-		empty.classList.remove('fade-in');
-		empty.classList.add('fade-out', 'animated');
-		data.classList.remove('fade-out');
-		data.classList.add('fade-in', 'animated');
-		data.classList.remove('hide');
-		empty.classList.add('hide');
-	} else {
-		data.classList.remove('fade-in');
-		data.classList.add('fade-out', 'animated');
-		empty.classList.remove('fade-out');
-		empty.classList.add('fade-in', 'animated');
-		empty.classList.remove('hide');
+	if (text === '') {
 		data.classList.add('hide');
+		empty.classList.remove('hide');
+	} else {
+		empty.classList.add('hide');
+		data.classList.remove('hide');
 	}
 }
 
 function encrypt() {
 	let plaintext = document.getElementsByTagName('textarea')[0].value;
-
-	showResult(plaintext)
+	showResult(plaintext);
 
 	plaintext = plaintext.replace(/a|e|i|o|u/gi, function (match) {
 		return password[match];
@@ -41,17 +44,33 @@ function encrypt() {
 
 function decrypt() {
 	let plaintext = document.getElementsByTagName('textarea')[0].value;
-
-	showResult(plaintext)
+	showResult(plaintext);
 
 	const reversedPassword = {};
 	Object.entries(password).forEach(([key, value]) => {
 		reversedPassword[value] = key;
 	});
 
-	plaintext = plaintext.replace(/ai|enter|imes|ober|ufat/gi, function (match) {
-		return reversedPassword[match];
-	});
+	plaintext = plaintext.replace(
+		/ai|enter|imes|ober|ufat/gi,
+		function (match) {
+			return reversedPassword[match];
+		}
+	);
 
-	output.innerHTML = plaintext;
+	if (plaintext !== document.getElementsByTagName('textarea')[0].value) {
+		output.innerHTML = plaintext;
+	} else {
+		showResult('');
+	}
+}
+
+async function copy() {
+	await navigator.clipboard.writeText(output.innerHTML);
+	copyButon.innerHTML = 'Mensagem Copiada';
+	copyButon.classList.add('copied');
+	setTimeout(() => {
+		copyButon.innerHTML = 'Copiar';
+		copyButon.classList.remove('copied');
+	}, 1500);
 }
